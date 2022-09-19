93be2fbb6bc143a9ec814b3620a296e39c5ab1ca
Roman Home <t89875041999@yandex.ru>
2022-09-19 22:36:41 +0300

    Changes to be committed: 	modified:   src/components/AddColorForm.jsx

--- src/components/AddColorForm.jsx
+++ src/components/AddColorForm.jsx
@@ -1,15 +1,63 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
-function AddColorForm() {
-  return (
+function AddColorForm({ onNewColor = f => f }) {
 
-        <form>
-            <input type="text" placeholder='color title...' required />                                                                                             />
-            <input type="color" required />
-            <button>ADD</button>
-        </form>
+    // Контролируемый компонент
 
-  )
+    const [title, setTitle] = useState("");
+    const [color, setColor] = useState("#000000");
+
+    const submit = (e) => {
+        e.preventDefault();
+        onNewColor(title, color)
+        setTitle("");
+        setColor("");
+        console.log(title, color);
+    }
+
+    return (
+        <form onSubmit={submit}>
+             <input 
+                value={title}
+                onChange={e => setTitle(e.target.value)}
+                //onChange={e => console.log(e.target.value)}
+                type="text" 
+                placeholder='color title...' 
+                required 
+                />
+             <input 
+                value={color}
+                onChange={e => setColor(e.target.value)}
+                type="color" 
+                required 
+                />
+             <button>ADD</button>
+         </form>
+    )
+    
+    // Императивный код (неконтролируемый) 
+    // const txtTitle = useRef();
+    // const hexColor = useRef();
+
+    // const submit = e => {
+    //     e.preventDefault();
+    //     const title = txtTitle.current.value;
+    //     const color = hexColor.current.value;
+    //     onNewColor(title, color);
+    //     txtTitle.current.value = "";
+    //     hexColor.current.value = "";
+    // }
+
+    // return (
+
+    //     <form onSubmit={submit}>
+    //         <input ref={txtTitle} type="text" placeholder='color title...' required />
+    //         <input ref={hexColor} type="color" required />
+    //         <button>ADD</button>
+    //     </form>
+
+    // )
+    
 }
 
 export default AddColorForm;
